440d6cf4d462e1dd91ee087f7832af61f2ece0cf
PavanAmbuskar <166595571+PavanAmbuskar@users.noreply.github.com>
2024-07-09 22:27:16 +0530

    script

--- 03_vardemo.sh
+++ 03_vardemo.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 name=pavan
 age=24
-echo "my name is $name & age is $age
+echo "my name is $name & age is $age"
 name=paul 
-echo "my name is $name & age is $age
+echo "my name is $name & age is $age"
+#var to store output of the command
+hostname=$(hostname)
+echo "name of the machine is $hostname"
