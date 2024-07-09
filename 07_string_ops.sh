2855c2ddfd35bfafeee7e271e8c8bd9ec0904bd0
PavanAmbuskar <166595571+PavanAmbuskar@users.noreply.github.com>
2024-07-09 23:33:13 +0530

    script

--- 07_string_ops.sh
+++ 07_string_ops.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 myvar="hey buddy,how are you??"
 myvarlength=${#myvar}
-echo "myvar length is $myvarlegth"
+echo "myvar length is $myvarlength"
 echo "uppercase is ${myvar^^}"
 echo "lowercase is ${myvar,,}"
+
+#to replace a word
+newvar=${myvar/buddy/pavan}
+echo "new var is $newvar"
+
+#to slice string
+echo "after slice ${myvar:4:5}"
