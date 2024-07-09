81dd2b88bdd23575881dc93e87d6fa19fc594405
PavanAmbuskar <166595571+PavanAmbuskar@users.noreply.github.com>
2024-07-09 21:48:55 +0530

    script

---
+++ 02_comments.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+this is my first comments
+
+# this is comment line
+<<comment
+this 
+is
+multi
+line comment
+comment
---
+++ 03_vardemo.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+name=pavan
+age=24
+echo "my name is $name & age is $age
+name=paul 
+echo "my name is $name & age is $age
