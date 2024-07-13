c7f962b81cb2e2f873dff833bd665473cb0f0acd
PavanAmbuskar <166595571+PavanAmbuskar@users.noreply.github.com>
2024-07-13 10:45:10 +0530

    project

---
+++ ram_status.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+freespace=free -mt | grep "Total" | awk '{print$4}'
+TH=500
+if[[ $freespace -lt $TH]]
+then
+echo "ram is running out" 
+else
+echo "ram space is sufficient $freespace"
+fi
