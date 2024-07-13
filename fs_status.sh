a914762fb344891339107289b07fdc9c6ef92d03
PavanAmbuskar <166595571+PavanAmbuskar@users.noreply.github.com>
2024-07-13 11:06:09 +0530

    pr

--- fs_status.sh
+++ fs_status.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 FU=$(df -H | egrep -v "Filesystem|none|drvfs" | grep "/usr/lib/wsl/lib" | awk '{print $5}'|tr -d %)
-if[[ $FU -gt 80 ]]
+if [[ $FU -gt 80 ]]
 then 
 echo "disc space running low $FU"
 else
